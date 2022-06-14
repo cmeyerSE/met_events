743f09fb32d9a563effb0149455dff3a89c84d65
Cory <cameyer182@gmail.com>
2022-06-14 15:37:03 -0400

    created lib folders for cli and made it executeble. add pry and nokogiri to the gemfile and installed gems. renamed the environment file.

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "met_events/version"
+require_relative "met_events/cli"
+
+require 'pry'
 
 module MetEvents
   class Error < StandardError; end
---
+++ lib/met_events/cli.rb
@@ -0,0 +1,5 @@
+class MetEvents::CLI
+    def call
+        puts 'Welcome to The Metropolitan Museum of Art!'
+    end
+end
