1aa89bc67d66efdf51cbb45ab77432532d881f92
Cory <cameyer182@gmail.com>
2022-06-15 16:11:30 -0400

    added validation check to make sure the data is input correctly

--- lib/met_events/cli.rb
+++ lib/met_events/cli.rb
@@ -1,5 +1,33 @@
 class MetEvents::CLI
     def call
-        puts 'Welcome to The Metropolitan Museum of Art!'
+        puts "\nWelcome to The Metropolitan Museum of Art!\n"
+        get_days
+        list_days
+        get_user_day
+        #get_events_for(day) 
+    end
+
+    def get_days
+        #to be scraped for days where events are listed
+        @days = ['1st', '4th', '7th', '13th', '21st']
+    end
+
+    def list_days
+        puts 'Choose a day to see event'
+        #list days
+        @days.each.with_index(1) do |day, index|
+            puts "#{index}. #{day}"
+        end
+    end
+
+    def get_user_day
+        chosen_day = gets.strip
+        binding.pry
+        # if valid_input(chosen_day, @days)
+        # end
+    end
+
+    def valid_input(input, data)
+        input.to_i <= data.length && input.to_i > 0
     end
 end
